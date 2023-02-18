b44c3507ee60664af5f54b1145d9ce04840ef7a9
Ahmed Fahmy <106992620+AhmedFahmy0@users.noreply.github.com>
2023-02-19 00:22:03 +0200

    Add files via upload

---
+++ assets/js/index_script.js
@@ -0,0 +1,11 @@
+var typed =  new Typed('#typed', {
+    strings: [' Developer', ' Freelancer.', ' Photographer.', ' Designer.'],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+});
+
+
+
+
